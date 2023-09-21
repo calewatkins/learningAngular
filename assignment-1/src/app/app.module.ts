2f72689bc48fc5cc0ea74464b8045c787b5ef5c3
Cale Watkins <caleawatkins@gmail.com>
2023-09-21 18:14:06 -0400

    first commit

---
+++ assignment-1/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningComponent,
+    SuccessComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, SuccessComponent]
+})
+export class AppModule { }
